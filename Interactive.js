779cd1e20382e547735898be2dc1c6cdd9641d67
Iqbalhoshen Saif <iqbalhoshensaif@gmail.com>
2024-01-28 21:22:01 +0600

    area

---
+++ Interactive.js
@@ -0,0 +1,95 @@
+function getInputValue(inputTextValue) {
+
+    const InputValue = document.getElementById(inputTextValue);
+    const InputText = InputValue.value;
+    const FloatValue = parseFloat(InputText);
+    InputValue.value = '';
+
+    return FloatValue;
+}
+
+function outputValues(shapeText, getArea, unit) {
+
+    const resultTextArea = document.getElementById('result');
+    resultTextArea.innerText = "=> " + shapeText + getArea + unit;
+
+}
+
+function nanCheck(input1, input2) {
+    if (isNaN(input1) || isNaN(input2) || input1 === '' || input2 === '') {
+        alert("Please enter valid numeric values!");
+        return true; // Exit function early
+    }
+}
+
+function TriangleArea() {
+    const base = getInputValue("TriangleInput1");
+    const height = getInputValue("TriangleInput2");
+    const check = nanCheck(base, height);
+
+    if (check === true) {
+        return;
+    }
+    const area = 0.5 * base * height;
+    outputValues("Triangle Area is : ", area, " cm²")
+}
+
+function RectangleArea() {
+    const w = getInputValue("RectangleInput1");
+    const l = getInputValue("RectangleInput2");
+    const check = nanCheck(w, l);
+
+    if (check === true) {
+        return;
+    }
+    const area = w * l;
+    outputValues("Rectangle Area is : ", area, " cm²")
+}
+
+function ParallelogramArea() {
+    const b = getInputValue("ParallelogramInput1");
+    const h = getInputValue("ParallelogramInput2");
+    const check = nanCheck(b, h);
+
+    if (check === true) {
+        return;
+    }
+    const area = b * h;
+    outputValues("Parallelogram Area is : ", area, " cm²")
+}
+
+function RhombusArea() {
+    const d1 = getInputValue("RhombusInput1");
+    const d2 = getInputValue("RhombusInput2");
+    const check = nanCheck(d1, d2);
+
+    if (check === true) {
+        return;
+    }
+    const area = 0.5 * d1 * d2;
+    outputValues("Rhombus Area is : ", area, " cm²")
+}
+
+function PentagonArea() {
+    const p = getInputValue("PentagonInput1");
+    const b = getInputValue("PentagonInput2");
+    const check = nanCheck(p, b);
+
+    if (check === true) {
+        return;
+    }
+    const area = 0.5 * p * b;
+    outputValues("Pentagon Area is : ", area, " cm²")
+
+}
+function EllipseArea() {
+    const a = getInputValue("EllipseInput1");
+    const b = getInputValue("EllipseInput2");
+    const check = nanCheck(a, b);
+
+    if (check === true) {
+        return;
+    }
+    const area = 3.1416 * a * b;
+    outputValues("Ellipse Area is : ", area, " cm²")
+}
